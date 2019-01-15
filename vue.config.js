b7e49828a553ba5110e25fd3ffa2b6bb785bb1c9
biezhenyu <biezhenyu521@163.com>
2019-01-15 11:09:18 +0800

    vue,vuex,axios等采用cdn方式

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 // 在控制台打印
 console.log('***************************')
 console.log('环境:' + process.env.NODE_ENV)
@@ -5,6 +7,43 @@ console.log('前端域名:' + process.env.VUE_APP_URL)
 console.log('接口地址:' + process.env.VUE_APP_APIURL)
 console.log('***************************')
 
+
+// cdn 预加载使用
+const externals = {
+  'vue': 'Vue',
+  'vue-router': 'VueRouter',
+  'vuex': 'Vuex',
+  'axios': 'axios',
+  'element-ui': 'ELEMENT'
+}
+
+const cdn = {
+  // 开发环境
+  dev: {
+    css: [
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+    ],
+    js: []
+  },
+  // 生产环境
+  build: {
+    css: [
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+    ],
+    js: [
+      'https://cdn.bootcss.com/vue/2.5.21/vue.min.js',
+      'https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js',
+      'https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
+      'https://cdn.bootcss.com/axios/0.18.0/axios.min.js',
+      'https://unpkg.com/element-ui/lib/index.js',
+    ]
+  }
+}
+
+// 是否使用gzip
+const productionGzip = false
+// 需要gzip压缩的文件后缀
+const productionGzipExtensions = ['js', 'css']
 
 module.exports = {
 
@@ -16,14 +55,58 @@ module.exports = {
   lintOnSave: true,
 
   // webpack配置
-  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-  chainWebpack: () => {},
-  configureWebpack: () => {},
+  chainWebpack: (config) => {
+
+    // 添加CDN参数到 htmlWebpackPlugin配置中， 详见public/index.html 修改
+    config.plugin('html').tap(args => {
+      if (process.env.NODE_ENV === 'production') {
+        args[0].cdn = cdn.build
+      }
+      if (process.env.NODE_ENV === 'development') {
+        args[0].cdn = cdn.dev
+      }
+      return args
+    })
+
+  },
+
+  // 修改webpack config, 使其不打包externals下的资源
+  configureWebpack: (config) => {
+
+    const myConfig = {}
+
+    if (process.env.NODE_ENV === 'production') {
+      // 1. 生产环境npm包转CDN
+      myConfig.externals = externals
+
+      myConfig.plugins = []
+
+      // 2. 构建时开启gzip，降低服务器压缩对CPU资源的占用，服务器也要相应开启gzip
+      productionGzip && myConfig.plugins.push(
+        new CompressionWebpackPlugin({
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 8192,
+          minRatio: 0.8
+        })
+      )
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+      /**
+       * 关闭host check，方便使用ngrok之类的内网转发工具
+       */
+      myConfig.devServer = {
+        disableHostCheck: true
+      }
+    }
+    return myConfig
+
+  },
 
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: true,
-  // css相关配置
 
+  // css相关配置
   css: {
    // 是否使用css分离插件 ExtractTextPlugin
    extract: true,
